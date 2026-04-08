2df68541e3d70539e6995fa6ae8c0aa2564c2646
Darsh505 <Darsh505@users.noreply.github.com>
2026-04-08 23:07:13 +0530

    fix: pass environment class instead of instance in app.py

--- triage_env/server/app.py
+++ triage_env/server/app.py
@@ -3,6 +3,6 @@ from openenv.core.env_server import create_web_interface_app
 from triage_env.models import MedAction, MedObservation
 from triage_env.server.triage_environment import TriageEnvironment
 
-env = TriageEnvironment()
+env_class = TriageEnvironment
 
-app = create_web_interface_app(env, MedAction, MedObservation)
+app = create_web_interface_app(env_class, MedAction, MedObservation)
